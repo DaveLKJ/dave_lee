60ec1f4ea4e422100ec66f46c746ea8fadc0ccd7
KongJian <kongjian3@hotmail.com>
2024-01-30 00:41:11 +0800

    pop up done

--- gaProject1/JS/mole.js
+++ gaProject1/JS/mole.js
@@ -1,1 +1,32 @@
+let popUpGrid;
+
+let timer = 60;
 let score = 0;
+
+window.onload = () => {
+    setGame();
+}
+
+function getRandomGrid() {
+    let randomNum = Math.floor(Math.random() * 9);
+    return randomNum.toString();
+}
+
+function popMole() {
+
+    if (popUpGrid) {
+        popUpGrid.removeChild(popUpGrid.firstChild);
+    }
+
+    let mole = document.createElement("img");
+    mole.src = './Assets/mole.png';
+
+    let num = getRandomGrid();
+    popUpGrid = document.getElementById(num);
+    popUpGrid.appendChild(mole);
+}
+
+function setGame() {
+    
+    setInterval(popMole, 2000);
+}
