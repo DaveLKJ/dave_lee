883ce5ac367bd95e286b38ceb1416c27a0c3da3a
KongJian <kongjian3@hotmail.com>
2024-02-02 00:20:29 +0800

    work done

--- gaProject1/JS/mole.js
+++ gaProject1/JS/mole.js
@@ -5,6 +5,9 @@ let timer;
 let score;
 let timerDisplay;
 let scoreDisplay;
+let gameTimer;
+let moleTimer;
+let bombTimer;
 
 window.onload = () => {
     gameOver = true;
@@ -16,11 +19,16 @@ window.onload = () => {
 
 function getRandomGrid() {
     let randomNum = Math.floor(Math.random() * 9);
+    console.log(randomNum);
     return randomNum.toString();
 }
 
 function popMole() {
-    if (popMoleGrid) {
+    if (gameOver) {
+        return;
+    }
+
+    if (popMoleGrid && popMoleGrid.firstChild) {
         popMoleGrid.removeChild(popMoleGrid.firstChild);
     }
 
@@ -28,12 +36,19 @@ function popMole() {
     mole.src = './Assets/mole.png';
 
     let num = getRandomGrid();
+    if (popBombGrid && popBombGrid.id === num) {
+        return;
+    }
     popMoleGrid = document.getElementById(num);
     popMoleGrid.appendChild(mole);
 }
 
 function popBomb() {
-    if (popBombGrid) {
+    if (gameOver) {
+        return;
+    }
+
+    if (popBombGrid && popBombGrid.firstChild) {
         popBombGrid.removeChild(popBombGrid.firstChild);
     }
 
@@ -41,35 +56,46 @@ function popBomb() {
     bomb.src = './Assets/bomb.png';
 
     let num = getRandomGrid();
-    // if (popMoleGrid.id = num) {
-    //     return;
-    // }
+    if (popMoleGrid && popMoleGrid.id === num) {
+        return;
+    }
     popBombGrid = document.getElementById(num);
     popBombGrid.appendChild(bomb);
 }
 
 function selectGrid() {
+    if (gameOver) {
+        return;
+    }
+
     if (this == popMoleGrid) {
         score += 1;
         scoreDisplay.textContent = `Score: ${score}`;
     }
+
+    if (this == popBombGrid) {
+        alert(`Game over! Your score is ${score}`);
+        clearInterval(gameTimer);
+        clearInterval(moleTimer);
+        clearInterval(bombTimer);
+    }
 }
 
 function setGame() {
+    gameOver = false;
+
     for (let i=0; i<9; i++) {
         let grid = document.getElementById(i)
         grid.addEventListener("click", selectGrid);
     }
-
-    gameOver = false;
+    
     timer = 30;
     timerDisplay.textContent = `Timer: ${timer}`;
     score = 0;
     scoreDisplay.textContent = `Score: ${score}`;
 
     gameTimer = setInterval(() => {
-        moleTimer = setInterval(popMole, 1000);
-        bombTimer = setInterval(popBomb, 1200);
+
         timer--;
         timerDisplay.textContent = `Timer: ${timer}`;
 
@@ -77,7 +103,11 @@ function setGame() {
             gameOver = true;
             clearInterval(gameTimer);
             clearInterval(moleTimer);
+            clearInterval(bombTimer);
             alert(`Game over! Your score is ${score}`);
         }
     }, 1000);
+
+    moleTimer = setInterval(popMole, 2000);
+    bombTimer = setInterval(popBomb, 1500);
 }
